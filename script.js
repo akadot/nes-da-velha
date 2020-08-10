86099a5bcfa0f9c582cca7036b71589f019a3cc1
Murilo <muriloholiveira@hotmail.com>
2020-08-09 22:10:21 -0300

    :tada: Game Functions Completed

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
   const squares = document.querySelectorAll(".board div");
   const playerDisplay = document.querySelector("#player");
+  const result = document.querySelector("#result");
+  const start = document.querySelector("#btn");
 
-  squares.forEach((square) => {
-    square.addEventListener("click", playerClick);
-  });
+  let currentPlayer = "cross";
 
-  let currentPlayer = "playerX";
   let winnerSequences = [
     [0, 1, 2],
     [3, 4, 5],
@@ -18,33 +17,54 @@ document.addEventListener("DOMContentLoaded", () => {
     [2, 4, 6],
   ];
 
+  let gameOver = false;
+
+  start.addEventListener("click", () => {
+    squares.forEach((item) => {
+      item.textContent = "";
+    });
+    gameOver = false;
+  });
+
+  squares.forEach((square) => {
+    square.addEventListener("click", playerClick);
+  });
+
   function playerClick(event) {
-    const squaresArray = Array.from(squares);
-    const index = squaresArray.indexOf(event.target);
-    playerDisplay.textContent = currentPlayer;
+    if (gameOver == false) {
+      const squaresArray = Array.from(squares);
+      const index = squaresArray.indexOf(event.target);
 
-    //const teste = checkWinner(squares[index].className);
+      if (currentPlayer === "cross" && squaresArray[index].textContent == "") {
+        squaresArray[index].style.color = "#50fa7b";
+        squaresArray[index].textContent = "X";
+        currentPlayer = "circle";
+      }
+      if (currentPlayer === "circle" && squaresArray[index].textContent == "") {
+        squaresArray[index].style.color = "#ff5555";
+        squaresArray[index].textContent = "O";
+        currentPlayer = "cross";
+      }
 
-    if (currentPlayer === "playerX") {
-      squares[index].classList.add("playerX");
-      currentPlayer = "playerO";
+      const pickWinner = checkWinner(squaresArray[index].textContent);
+      if (pickWinner === true) {
+        result.textContent += squaresArray[index].textContent;
+        gameOver = true;
+      }
     } else {
-      squares[index].classList.add("playerO");
-      currentPlayer = "playerX";
+      return;
     }
   }
 
   function checkWinner(player) {
     for (i in winnerSequences) {
       if (
-        squares[winnerSequences[i][0]].className == player &&
-        squares[winnerSequences[i][1]].className == player &&
-        squares[winnerSequences[i][2]].className == player
+        squares[winnerSequences[i][0]].textContent == player &&
+        squares[winnerSequences[i][1]].textContent == player &&
+        squares[winnerSequences[i][2]].textContent == player
       ) {
-        console.log(i);
-        return i;
+        return true;
       }
     }
-    return -1;
   }
 });
